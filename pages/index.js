25eefc38e9a1c7a0b6d23b34fa0bb1c73c117355
Eliseu Videira <eliseuvideira1@gmail.com>
2020-04-10 17:08:50 -0300

    API Routes

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,56 @@
 import React from 'react';
-import Layout from '../components/Layout';
-import Link from 'next/link';
-import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
 
-const getPosts = () => {
-  return [
-    { id: 'hello-nextjs', title: 'Hello Next.js' },
-    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
-    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' },
-  ];
+const fetcher = (url) => {
+  return fetch(url).then((res) => res.json());
 };
 
-const PostLink = ({ post }) => (
-  <>
-    <li key={post.id}>
-      <Link href="/p/[id]" as={`/p/${post.id}`}>
-        <a>{post.title}</a>
-      </Link>
-    </li>
-    <style jsx>{`
-      h1,
-      a {
-        font-family: 'Arial';
-      }
+const Index = () => {
+  const { query } = useRouter();
+  const { data, error } = useSWR(
+    `/api/randomQuote${query.author ? '?author=' + query.author : ''}`,
+    fetcher,
+  );
+  const author = data && data.author;
+  let quote = data && data.quote;
 
-      ul {
-        padding: 0;
-      }
+  if (!data) {
+    quote = 'Loading...';
+  }
 
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
+  if (error) {
+    quote = 'Failed to fetch the quote.';
+  }
 
-      a {
-        text-decoration: none;
-        color: blue;
-      }
+  return (
+    <main className="center">
+      <div className="quote">{quote}</div>
+      {author && <span className="author">- {author}</span>}
 
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
-  </>
-);
-
-PostLink.propTypes = {
-  post: PropTypes.object.isRequired,
+      <style jsx>
+        {`
+          main {
+            width: 90%;
+            max-width: 900px;
+            margin: 300px auto;
+            text-align: center;
+          }
+          .quote {
+            font-family: cursive;
+            color: #e243de;
+            font-size: 24px;
+            padding-bottom: 10px;
+          }
+          .author {
+            font-family: sans-serif;
+            color: #559834;
+            font-size: 20px;
+          }
+        `}
+      </style>
+    </main>
+  );
 };
 
-const Blog = () => (
-  <Layout>
-    <h1>My Blog</h1>
-    <ul>
-      {getPosts().map((post) => (
-        <PostLink key={post.id} post={post} />
-      ))}
-    </ul>
-  </Layout>
-);
-
-export default Blog;
+export default Index;
